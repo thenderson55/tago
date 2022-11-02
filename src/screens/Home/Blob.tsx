efcf1aded50357c1f20e1a4224216bf3ee215bf1
Thomas Henderson <tbhenderson55@yahoo.com>
2022-11-02 09:26:10 +0700

    blob is moving

--- src/screens/Home/Blob.tsx
+++ src/screens/Home/Blob.tsx
@@ -1,11 +1,13 @@
 import {
   Canvas,
   Path,
+  useClockValue,
   useComputedValue,
   useValue,
 } from '@shopify/react-native-skia';
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import {createNoise2D} from 'simplex-noise';
 import theme from '../../theme';
 import {spline} from '../../utils/spline';
 
@@ -42,12 +44,54 @@ function createPoints() {
   return points;
 }
 
+function map(
+  n: number,
+  start1: number,
+  end1: number,
+  start2: number,
+  end2: number,
+) {
+  return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+}
+
 function Blob() {
   const points = useValue(createPoints());
+  const clock = useClockValue();
+  const hueNoiseOffset = useValue(0);
+  const noise = createNoise2D();
+  const noiseStep = 0.005;
+
+  const animate = () => {
+    const newPoints = [];
+
+    for (let i = 0; i < points.current.length; i++) {
+      const point = points.current[i];
+
+      // return a pseudo random value between -1 / 1 based on this point's current x, y positions in "time"
+      const nX = noise(point.noiseOffsetX, point.noiseOffsetX);
+      const nY = noise(point.noiseOffsetY, point.noiseOffsetY);
+      // map this noise value to a new value, somewhere between it's original location -20 and it's original location + 20
+      const x = map(nX, -1, 1, point.originX - 20, point.originX + 20);
+      const y = map(nY, -1, 1, point.originY - 20, point.originY + 20);
+
+      // update the point's current coordinates
+      point.x = x;
+      point.y = y;
+
+      // progress the point's x, y values through "time"
+      point.noiseOffsetX += noiseStep;
+      point.noiseOffsetY += noiseStep;
+
+      newPoints.push(point);
+    }
+
+    points.current = newPoints;
+  };
 
   const path = useComputedValue(() => {
+    animate();
     return spline(points.current, 1, true);
-  }, []);
+  }, [clock]);
 
   return (
     <View style={styles.container}>
