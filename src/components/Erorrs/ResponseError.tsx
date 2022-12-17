729db115a577f287f80abc47508c187e9453e737
Thomas Henderson <tbhenderson55@yahoo.com>
2022-12-17 08:04:58 +0000

    errors

--- src/components/Erorrs/ResponseError.tsx
+++ src/components/Erorrs/ResponseError.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+
 interface Props {
-  touched: boolean | undefined;
   message: string | undefined;
   spaceFiller?: boolean;
 }
 
-function FormError(props: Props) {
-  const {touched, message, spaceFiller} = props;
-  if (touched && message) {
+function ResponseError(props: Props) {
+  const {message, spaceFiller} = props;
+  if (message) {
     return (
       <View>
         <Text style={styles.error}>{message}</Text>
@@ -23,8 +23,8 @@ function FormError(props: Props) {
 }
 
 const styles = StyleSheet.create({
-  error: {color: 'red', fontSize: 12, height: 20, marginTop: 5},
+  error: {color: 'red', fontSize: 16, height: 20, marginTop: 20},
   filler: {height: 20},
 });
 
-export default FormError;
+export default ResponseError;
