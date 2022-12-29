62a72616faf2a5a4c844b34878370744b014af9d
Thomas Henderson <tbhenderson55@yahoo.com>
2022-12-29 13:28:24 +0000

    tab basic styling

--- src/TabScreen.tsx
+++ src/TabScreen.tsx
@@ -4,13 +4,33 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import PhotosStack from './stacks/Photos/PhotosStack';
 import HomeStack from './stacks/Home/HomeStack';
 import addGlobalRoutes from './stacks/addGlobalRoutes';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import theme from './theme';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function BottomTabs() {
   return (
-    <Tab.Navigator initialRouteName="Home">
+    <Tab.Navigator
+      initialRouteName="Home"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName = '';
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Photos') {
+            iconName = focused ? 'images' : 'images-outline';
+            // iconName = focused ? 'ios-list' : 'ios-list-outline';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.grey,
+      })}>
       <Tab.Screen
         options={() => ({
           headerShown: false,
