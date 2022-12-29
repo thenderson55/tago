5975ae722d74557e86c582132b7cbaecff0c6ddd
Thomas Henderson <tbhenderson55@yahoo.com>
2022-12-29 10:51:49 +0000

    flatlist

--- src/screens/Photos/PhotosListScreen.tsx
+++ src/screens/Photos/PhotosListScreen.tsx
@@ -1,40 +1,28 @@
 import React from 'react';
 import {
+  FlatList,
+  // RefreshControl,
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  VirtualizedList,
 } from 'react-native';
 import CardPhotoList from '../../components/Cards/CardPhotoList';
-import usePhotosStore, {PhotoType} from '../../store/usePhotosStore';
-
-const getItem = (data: PhotoType[], index: number) => {
-  // console.log('getItem data', data);
-  return {
-    id: Math.random().toString(12).substring(0),
-    category: data[index].category,
-    title: data[index].title,
-    description: data[index].description,
-    url: data[index].url,
-    location: data[index].location,
-    ref: data[index].ref,
-    created: data[index].created,
-  };
-};
+import usePhotosStore from '../../store/usePhotosStore';
 
 function PhotosListScreen() {
   const {photos} = usePhotosStore();
-  const getItemCount = () => photos.length;
 
   return (
     <SafeAreaView style={styles.container}>
-      <VirtualizedList
+      <FlatList
+        // style={styles.list}
+        // contentContainerStyle={styles.listContent}
+        // refreshControl={
+        //   <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        // }
         data={photos}
-        initialNumToRender={4}
         renderItem={({item}) => <CardPhotoList item={item} />}
         keyExtractor={item => item.ref}
-        getItemCount={getItemCount}
-        getItem={getItem}
       />
     </SafeAreaView>
   );
